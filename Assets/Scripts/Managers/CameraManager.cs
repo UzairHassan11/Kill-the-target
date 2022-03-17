665d170570906ffb3b2d9dadbc20cb21fed6c110
Uzair Hassan <uzairhassan11@gmail.com>
2022-03-17 23:03:01 +0500

    Initial commit

---
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    
+    #region singleton
+
+    public static CameraManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+    
+    [SerializeField] Animator animator;
+
+    public void SetAnimatorState(CamStates camState)
+    {
+        animator.CrossFade(camState.ToString(), .1f);
+    }
+    
+    public void SetAnimatorState(int index)
+    {
+        animator.CrossFade(((CamStates)index).ToString(), .1f);
+    }
+    
+    public void SetAnimatorState(string state)
+    {
+        animator.CrossFade(state, .1f);
+    }
+}
+[System.Serializable]
+public enum CamStates
+{
+    start,
+    gameplay,
+    endPoint,
+    finalMomentum
+}
---
+++ Assets/Scripts/Managers/UiManager.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+// using ElephantSDK;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+// using GameAnalyticsSDK;
+
+public class UiManager : MonoBehaviour
+{
+    #region .
+    int LevelNumberPref
+    {
+        get { return SaveData.Instance.LevelNumberPref; }
+        set
+        {
+            SaveData.Instance.LevelNumberPref = value;
+            SaveSystem.SaveProgress(); 
+        }
+    }
+    
+    int LevelNumberAnalytics
+    {
+        get { return SaveData.Instance.LevelNumberAnalytics; }
+        set
+        {
+            SaveData.Instance.LevelNumberAnalytics = value;
+            SaveSystem.SaveProgress(); 
+        }
+    }
+    #endregion
+
+    [SerializeField] private Text levelText;
+    
+    [SerializeField] private bool test;
+    
+    [SerializeField] private GameObject startPanel, winPanel, failPanel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelText.text = "Level " + LevelNumberAnalytics.ToString("00");
+        if (!test)
+        {
+            // Elephant.LevelStarted(LevelNumberAnalytics);
+            // GameAnalytics.NewProgressionEvent(GAProgressionStatus.Start, "LevelStart", LevelNumberAnalytics);
+        }
+    }
+
+
+    public void OnClickStartPanel()
+    {
+        startPanel.SetActive(false);
+        CameraManager.instance.SetAnimatorState(CamStates.gameplay);
+        GameManager.instance.ChangeGameState(GameState.Running);
+        if (!test)
+        {
+            CameraManager.instance.SetAnimatorState(CamStates.gameplay);
+            GameManager.instance.ChangeGameState(GameState.Running);
+        }
+    }
+
+    public void ShowWinPanel(float delay = 0)
+    {
+        if (delay > 0)
+        {
+            StartCoroutine(showPanelWithDelay(delay, true));
+            return;
+        }
+        
+        if (!test)
+        {
+            // Elephant.LevelCompleted(LevelNumberAnalytics);
+            // GameAnalytics.NewProgressionEvent(GAProgressionStatus.Complete, "Level_Completed", LevelNumberAnalytics);
+        }
+
+        winPanel.SetActive(true);
+        LevelNumberAnalytics++;
+        LevelNumberPref++;
+    }
+    
+    public void ShowFailPanel(float delay = 0)
+    {
+        if (delay > 0)
+        {
+            StartCoroutine(showPanelWithDelay(delay, false));
+            return;
+        }
+        
+        if (!test)
+        {
+            // Elephant.LevelFailed(LevelNumberAnalytics);
+            // GameAnalytics.NewProgressionEvent(GAProgressionStatus.Fail, "Level_Failed", LevelNumberAnalytics);
+        }
+
+        failPanel.SetActive(true);
+    }
+
+    IEnumerator showPanelWithDelay(float delay, bool win)
+    {
+        yield return new WaitForSeconds(delay);
+        if (win) 
+            ShowWinPanel(0);
+        else
+            ShowFailPanel(0);
+    }
+
+    public void OnReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
